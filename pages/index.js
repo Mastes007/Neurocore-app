f06307aed502960256bd96bb9937891b88259bbb
Mastes007 <05ramalnzrv@gmail.com>
2025-08-01 02:38:10 +0400

    Move index.js into pages folder

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div style={{ background: '#000', color: '#0f0', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <h1>🧠 Welcome to Neurocore</h1>
+    </div>
+  );
+}
