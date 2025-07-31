1b66a0c9ab33795fdf00e657875f63f4f4512516
Mastes007 <05ramalnzrv@gmail.com>
2025-08-01 02:27:55 +0400

    Delete pages/index.js

--- pages/index.js
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ background: '#000', color: '#0f0', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      <h1>🧠 Welcome to Neurocore</h1>
-    </div>
-  );
-}
